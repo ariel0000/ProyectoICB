48687eb53c4c5ad32500acfe61b5c10e7e7d6645
Ariel Rosa <ariel.rosa@edit.com.ar>
2023-01-26 12:38:56 -0300

    intento de update vio_notificacion desde Chat y +

---
+++ app/Mensajeria.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Mensajeria extends React.Component{
+
+    render(){
+        return(
+            <div className="infoApp">
+              
+                <h4>Lugar para poder intercambiar mensajes entre usuarios</h4>
+                <br/>
+                <p>Busca la persona a la que quieras enviar un mensaje. Las últimas interacciones aparecerán aquí abajo</p>
+                
+            </div>
+        )
+    }
+
+}
+export default Mensajeria
